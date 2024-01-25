95c95c8a2b7daf9de132762ea9d68b04493228c0
terrywh <terry.wuhao@gmail.com>
2024-01-25 16:27:55 +0800

    更新 svelte 版本;

--- www/shell/shell_terminal.svelte
+++ www/shell/shell_terminal.svelte
@@ -1,5 +1,3 @@
-<div id="terminal" style="width: 100%; height: 100%;"></div>
-
 <script>
     import { onMount } from "svelte";
     import { Terminal } from "https://esm.sh/xterm@5.3.0";
@@ -10,38 +8,38 @@
     /** @type {Terminal} */
     let iTerminal;
     let iFitAddon;
-    onMount(async function() {
+    onMount(async function () {
         iTerminal = new Terminal({
             theme: {
-                foreground: '#c5c8c6',
-                background: '#161719',
-                cursor: '#d0d0d0',
+                foreground: "#c5c8c6",
+                background: "#161719",
+                cursor: "#d0d0d0",
 
-                black: '#000000',
-                brightBlack: '#000000',
+                black: "#000000",
+                brightBlack: "#000000",
 
-                red: '#fd5ff1',
-                brightRed: '#fd5ff1',
+                red: "#fd5ff1",
+                brightRed: "#fd5ff1",
 
-                green: '#87c38a',
-                brightGreen: '#94fa36',
+                green: "#87c38a",
+                brightGreen: "#94fa36",
 
-                yellow: '#ffd7b1',
-                brightYellow: '#f5ffa8',
+                yellow: "#ffd7b1",
+                brightYellow: "#f5ffa8",
 
-                blue: '#85befd',
-                brightBlue: '#96cbfe',
+                blue: "#85befd",
+                brightBlue: "#96cbfe",
 
-                magenta: '#b9b6fc',
-                brightMagenta: '#b9b6fc',
+                magenta: "#b9b6fc",
+                brightMagenta: "#b9b6fc",
 
-                cyan: '#85befd',
-                brightCyan: '#85befd',
+                cyan: "#85befd",
+                brightCyan: "#85befd",
 
-                white: '#e0e0e0',
-                brightWhite: '#e0e0e0'
+                white: "#e0e0e0",
+                brightWhite: "#e0e0e0",
             },
-            cursorStyle: 'bar',
+            cursorStyle: "bar",
             // fontFamily: "Cascadia Mono",
             fontFamily: "Intel One Mono",
             // fontFamily: "Sarasa Term SC",
@@ -49,26 +47,25 @@
             fontSize: 16,
             lineHeight: 1.2,
         });
-        iTerminal.open(document.getElementById('terminal'));
+        iTerminal.open(document.getElementById("terminal"));
         iFitAddon = new FitAddon();
         iTerminal.loadAddon(iFitAddon);
         iTerminal.loadAddon(new (WebglAddon.WebglAddon || WebglAddon)());
         iTerminal.focus();
-
-        iTerminal.onResize(async function(e) {
+        iTerminal.onResize(async function (e) {
             const rsp = await fetch(`/bash/resize?key=${key}`, {
                 method: "POST",
                 headers: {
                     "content-type": "application/json",
                 },
                 body: JSON.stringify({
-                    "rows": e.rows,
-                    "cols": e.cols, 
-                })
+                    rows: e.rows,
+                    cols: e.cols,
+                }),
             });
         });
 
-        const done = iTerminal.onTitleChange(function(e) {
+        const done = iTerminal.onTitleChange(function (e) {
             fit();
             done.dispose();
         });
@@ -77,7 +74,7 @@
     let fitTimeout;
     export function fit(cb) {
         clearTimeout(fitTimeout);
-        fitTimeout = setTimeout(function() {
+        fitTimeout = setTimeout(function () {
             iFitAddon.fit();
             console.log("terminal fit: ", iTerminal.rows, "x", iTerminal.cols);
             if (cb instanceof Function) cb();
@@ -93,10 +90,11 @@
     }
     let refreshTimeout;
     function refresh() {
+        console.log("refresh1");
         clearTimeout(refreshTimeout);
-        refreshTimeout = setTimeout(function() {
-            iTerminal.write('\0');
-            console.log("refresh");
+        refreshTimeout = setTimeout(function () {
+            iTerminal.write("\0");
+            if (refreshing) refresh();
         }, 30000);
     }
 
@@ -104,5 +102,6 @@
         if (refreshing) refresh();
         else clearTimeout(refreshTimeout);
     });
-
 </script>
+
+<div id="terminal" style="width: 100%; height: 100%;"></div>
